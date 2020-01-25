1fbe68b165b5293dfc77bf81df3fd53ff31246b0
hololeap <hololeap@gmail.com>
2020-01-24 21:32:42 -0700

    Initial commit

---
+++ example/Main.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Reflex.Conduit (
+      ResetConduitEvent (ResetConduitEvent)
+    , ClearInputEvent (ClearInputEvent)
+    , runConduitReflex)
+
+import Control.Monad.Fix (MonadFix)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.NodeId (MonadNodeId)
+import Data.Attoparsec.Text (Parser, string)
+import Data.Char (toUpper)
+import Data.Conduit.Attoparsec (ParseError, PositionRange, conduitParserEither)
+import Data.Function ((&))
+import Data.Functor (void)
+import Reflex.Class ( Reflex, Behavior, Event, MonadHold, accumB, fmapMaybe
+                    , leftmost, hold, never)
+import Reflex.PerformEvent.Class (PerformEvent, Performable)
+import Reflex.TriggerEvent.Class (TriggerEvent)
+import Reflex.Vty.Host (VtyEvent)
+import Reflex.Vty.Widget ( BoxStyle, VtyWidget, box, doubleBoxStyle, input
+                         , keyCombo, mainWidget, splitV, text)
+import Reflex.Vty.Widget.Layout(Orientation (Orientation_Column), fixed, runLayout)
+import qualified Data.Text as T
+import           Data.Text (Text)
+import qualified Graphics.Vty as V
+
+
+main :: IO ()
+main = mainWidget $ do
+    i  <- input
+    bs <- myBoxStyle
+    let charE = charEvent i
+    clearE <- void <$> keyCombo (V.KEsc, [])
+    resultE <- parseEvent myParser clearE charE
+--    resultE <- foo charE
+    runLayout (pure Orientation_Column) 0 never $
+        fixed (pure 5) $ box bs $ layout clearE charE resultE
+    void <$> keyCombo (V.KChar 'c', [V.MCtrl])
+  where
+    myParser = string "abc"
+    layout clearE charE resultE = 
+        myVLayout
+            (text "Matching string is \"abc\"; Press Esc to clear; Ctrl+C to quit")
+            (inputWithClear clearE charE)
+            (parseWidget clearE resultE)
+
+
+parseWidget :: (MonadHold t m, Show a, Reflex t) => Event t () -> Event t a -> VtyWidget t m ()
+parseWidget clearE resultE = do
+    let clearFE = "" <$ clearE
+    let showFE  = showText <$> resultE
+    pwE <- hold "" $ leftmost [clearFE, showFE]
+    text pwE
+
+parseEvent :: ( TriggerEvent t m, PerformEvent t m, MonadIO (Performable m), MonadIO m, Show a)
+    => Parser a -> Event t () -> Event t Char -> m (Event t (Either ParseError (PositionRange, a)))
+parseEvent p clearE charE =
+    runConduitReflex 
+        (ResetConduitEvent clearE) 
+        (ClearInputEvent clearE)
+        (T.singleton <$> charE) 
+        (conduitParserEither p)
+    
+
+inputWithClear :: ( Reflex t, MonadHold t m, MonadFix m)
+    => Event t () -> Event t Char -> VtyWidget t m ()
+inputWithClear clearE charE = do
+    let clearFE = const "" <$ clearE
+    let snocFE  = flip T.snoc <$> charE
+    inWCB <- accumB (&) "" $ leftmost [clearFE, snocFE]
+    text inWCB
+
+charEvent :: Reflex t => Event t VtyEvent -> Event t Char
+charEvent = fmapMaybe vtyEventToChar
+
+vtyEventToChar :: VtyEvent -> Maybe Char
+vtyEventToChar = \case
+    V.EvKey (V.KChar c) [V.MShift] -> Just $ toUpper c
+    V.EvKey (V.KChar c) [        ] -> Just $         c
+    _                              -> Nothing
+
+myBoxStyle :: (Reflex t, MonadHold t m, MonadFix m) => m (Behavior t BoxStyle)
+myBoxStyle = pure $ pure doubleBoxStyle
+
+showText :: Show a => a -> Text
+showText = T.pack . show
+
+myVLayout :: (Reflex t, Monad m, MonadNodeId m)
+    => VtyWidget t m a -> VtyWidget t m b -> VtyWidget t m c -> VtyWidget t m ()
+myVLayout wa wb wc = void $ splitV constSize noFocus wa $ splitV constSize noFocus wb wc
+  where
+    constSize = pure (const 1)
+    noFocus = pure (False, False)
+
---
+++ src/Reflex/Conduit.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Reflex.Conduit (
+      ResetConduitEvent (..)
+    , ClearInputEvent (..)
+    , runConduitReflex
+    ) where    
+
+import Control.Concurrent.Async (Async, async, cancel)
+import Control.Concurrent.STM.TChan (TChan, newTChanIO, writeTChan, readTChan, tryReadTChan)
+import Control.Concurrent.STM.TMVar (TMVar, newEmptyTMVarIO, putTMVar, takeTMVar, tryTakeTMVar)
+import Control.Monad (forever, (<=<))
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.STM (STM, atomically)
+import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
+import Data.Conduit (ConduitT, runConduit, awaitForever, yield, (.|))
+import Data.Functor (void)
+import Reflex.Class (Event)
+import Reflex.PerformEvent.Class (PerformEvent, Performable, performEvent_)
+import Reflex.TriggerEvent.Class (TriggerEvent, newEventWithLazyTriggerWithOnComplete)
+
+newtype ResetConduitEvent t = 
+    ResetConduitEvent { getResetConduitEvent :: Event t () }
+newtype ClearInputEvent t =
+    ClearInputEvent { getClearInputEvent :: Event t () }
+
+runConduitReflex ::
+    (TriggerEvent t m, PerformEvent t m, MonadIO (Performable m), MonadIO m)
+    => ResetConduitEvent t -> ClearInputEvent t -> Event t i 
+    -> ConduitT i o IO () -> m (Event t o)
+runConduitReflex resetEvent clearEvent inEvent conduit = do
+    inChan         <- liftIO newTChanIO
+    innerAsyncMVar <- liftIO newEmptyTMVarIO
+
+    writeEventToChan inEvent inChan
+    cancelOnResetEvent resetEvent innerAsyncMVar
+    clearOnClearEvent  clearEvent inChan
+
+    asyncRunConduit inChan innerAsyncMVar conduit
+
+
+
+-- | Calls `runConduit` asynchronously. Input coming in from the TChan will
+-- |   be passed to the Conduit. Whenever the Conduit presents output, the
+-- |   resulting Event will be fired.
+-- | 
+-- | Internally: The inner Async is pushed to the TMVar whenever it is created.
+-- |   (This allows for it to be cancelled by another thread.)
+-- |
+-- | When the Event is no longer needed, the outer Async will be cancelled as
+-- |   well as the inner Async (if it exits)
+asyncRunConduit :: (TriggerEvent t m)
+    => TChan i -> TMVar (Async ()) -> ConduitT i o IO () -> m (Event t o)
+asyncRunConduit inChan innerAsyncMVar conduit =
+    newEventWithLazyTriggerWithOnComplete $ \trigger -> do
+        outerAsync <- (async . forever) $ do
+            innerAsync <- (async . runConduit) $
+                   (forever $ (liftIO . atomically . readTChan) inChan >>= yield)
+                .| conduit
+                .| awaitForever (\o -> liftIO $ trigger o $ pure ())
+            atomically $ putTMVar innerAsyncMVar innerAsync
+        pure $ do
+            maybeInnerAsync <- atomically $ tryTakeTMVar innerAsyncMVar
+            maybe (pure ()) cancel maybeInnerAsync
+            cancel outerAsync
+    
+-- | Whenever the passed Event is fired, the TChan will be cleared.
+clearOnClearEvent :: (PerformEvent t m, MonadIO (Performable m))
+    => ClearInputEvent t -> TChan i -> m ()
+clearOnClearEvent (ClearInputEvent clearE) inChan =
+    performEvent_ $ (liftIO . atomically . clearTChan) inChan <$ clearE
+
+-- | Whenver the passed Event is fired, the Async will be taken from the passed
+-- | TMVar; `cancel` will then be called on the Async.
+cancelOnResetEvent :: (PerformEvent t m, MonadIO (Performable m))
+    => ResetConduitEvent t -> TMVar (Async ()) -> m ()
+cancelOnResetEvent (ResetConduitEvent resetE) tmvar =
+    performEvent_ $ (liftIO . (cancel <=< atomically . takeTMVar)) tmvar <$ resetE
+
+-- | Whenver the Event fires, its value will be pushed onto the TChan
+writeEventToChan :: (PerformEvent t m, MonadIO (Performable m))
+    => Event t a -> TChan a -> m ()
+writeEventToChan inEvent tchan = 
+    performEvent_ $ liftIO . atomically . writeTChan tchan <$> inEvent
+
+-- | Clear a TChan (read all elements)
+clearTChan :: TChan a -> STM ()
+clearTChan = void . runMaybeT . mapM_ (MaybeT . tryReadTChan) . repeat
